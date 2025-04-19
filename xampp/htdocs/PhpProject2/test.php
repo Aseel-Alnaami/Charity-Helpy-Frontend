e9e9a1426432645ad4b29ca2e456d7c44583055d
Aseel Alnaami <aseelalnaami1@gmail.com>
2025-04-19 16:39:09 +0300

    Delete xampp directory

--- xampp/htdocs/PhpProject2/test.php
+++
@@ -1,9 +0,0 @@
-<form method="post" action="do_absence.php">
-    <input type="text" name="cor" value="1"></input>
-    <input type="text" name="class" value="1"></input>
-    <input type="text" name="date" value="2024-04-28"></input>
-    <input type="text" name="act" value="INSERT"></input>
-    <input type="text" name="studentlist" value="2023100,2023101"></input>
-    <input type="submit">
-</form>
-
